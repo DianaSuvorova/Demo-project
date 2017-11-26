87fa9b2ba9d911fc856c7276098b781e63c95506
Diana Suvorova <diana.suvorova@uber.com>
2017-11-26 10:14:13 -0800

    karnma setup

---
+++ frontend/src/index.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15.4';
+import App from './containers/App';
+import './index.css';
+import store from './store';
+
+describe('App', () => {
+  it('runs', () => {
+    Enzyme.configure({ adapter: new Adapter() });
+
+    const wrap = (<Provider store={store}><App />< /Provider>);
+
+    const el = Enzyme.mount(wrap, { attachTo: document.getElementById('root') });
+
+    const textField = el.findWhere(c => c.name() === 'TextField');
+    const input = textField.findWhere(c => c.name() === 'input');
+    const button = el.findWhere(c => c.name() === 'button');
+    window.els = {
+      el,
+      textField,
+      input,
+      button,
+    };
+
+    const actions = [
+      () => input.getDOMNode().focus(),
+      () => (input.instance().value = 'D'),
+      () => (input.instance().value = 'Da'),
+      () => (input.instance().value = 'Dav'),
+      () => (input.instance().value = 'Davi'),
+      () => (input.instance().value = 'David'),
+      () => input.simulate('change', input),
+      () => button.simulate('click', button),
+    ];
+
+    actions.forEach((a, n) => {
+      setTimeout(() => a(), (n * 250) + 3000);
+    });
+  });
+});
